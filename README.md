db6bfe328a7dd18465c4d1d27d06c559ded4c81d
Jesse Lucas <jesse@jesselucas.com>
2021-03-28 11:09:49 -0400

    fork of rsc's qrweb to run locally

---
+++ README.md
@@ -0,0 +1,1 @@
+Fork of https://github.com/rsc/swtch/tree/master/qrweb
---
+++ cmd/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"runtime/debug"
+
+	qrweb "rsc.io/swtch/qrweb"
+)
+
+func carp(f http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Content-Type", "text/plain")
+				fmt.Fprintf(w, "<pre>\npanic: %s\n\n%s\n", err, debug.Stack())
+			}
+		}()
+		f.ServeHTTP(w, req)
+	})
+}
+
+func init() {
+	//	http.Handle("/qr/bits", carp(qrweb.Bits))
+	http.Handle("/qr/frame", carp(qrweb.Frame))
+	http.Handle("/qr/frames", carp(qrweb.Frames))
+	http.Handle("/qr/mask", carp(qrweb.Mask))
+	http.Handle("/qr/masks", carp(qrweb.Masks))
+	http.Handle("/qr/arrow", carp(qrweb.Arrow))
+	http.Handle("/qr/draw", carp(qrweb.Draw))
+	http.Handle("/qr/bitstable", carp(qrweb.BitsTable))
+	http.Handle("/qr/encode", carp(qrweb.Encode))
+	http.Handle("/qr/show/", carp(qrweb.Show))
+}
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	})
+	//	http.Handle("/qr/mask", carp(qrweb.Mask))
+	//	http.Handle("/qr/masks", carp(qrweb.Masks))
+	//	http.Handle("/qr/arrow", carp(qrweb.Arrow))
+	//	http.Handle("/qr/draw", carp(qrweb.Draw))
+	//	http.Handle("/qr/bitstable", carp(qrweb.BitsTable))
+	//	http.Handle("/qr/encode", carp(qrweb.Encode))
+	//	http.Handle("/qr/show/", carp(qrweb.Show))
+
+	fmt.Println("Running server at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
